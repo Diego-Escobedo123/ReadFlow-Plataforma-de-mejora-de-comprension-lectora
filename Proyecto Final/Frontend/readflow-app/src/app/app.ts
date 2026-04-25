2f27745a0959a213123f8fef61dc083e1457650f
Diego-Escobedo123 <diego.escobedo@ufm.edu>
2026-04-25 16:47:28 -0600

    resolve routing conflict

--- Proyecto Final/Frontend/readflow-app/src/app/app.ts
+++ Proyecto Final/Frontend/readflow-app/src/app/app.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,3 +11,17 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title = signal('readflow-app');
 }
+=======
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./pages/home/home";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {}
+>>>>>>> feat/routing-setup
---
+++ Proyecto Final/Frontend/readflow-app/src/app/pages/textos/textos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textos',
+  imports: [],
+  templateUrl: './textos.html',
+  styleUrl: './textos.css',
+})
+export class TextosComponent {
+
+}

3f5e0f68bf31dea0f5f508154f6c9cb7b620a349
Diego-Escobedo123 <diego.escobedo@ufm.edu>
2026-04-26 11:47:42 -0600

    HomePage component

--- Proyecto Final/Frontend/readflow-app/src/app/app.ts
+++ Proyecto Final/Frontend/readflow-app/src/app/app.ts
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App {
-  protected readonly title = signal('readflow-app');
-}
-=======
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home";
@@ -24,4 +10,3 @@ import { HomeComponent } from "./pages/home/home";
   styleUrl: './app.css'
 })
 export class App {}
->>>>>>> feat/routing-setup
